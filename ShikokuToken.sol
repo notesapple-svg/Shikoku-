4e7fd81f29e3228d22ede8259f0681521b62c93e
notesapple-svg <notesapple@outlook.com>
2026-02-20 19:15:20 -0600

    Update ShikokuToken.sol to add burn and mint functions with access control

---
+++ ShikokuToken.sol
@@ -0,0 +1,20 @@
+// Solidity version here
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ShikokuToken is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("Shikoku", "SHIK") {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public onlyOwner {
+        _burn(msg.sender, amount);
+    }
+}
